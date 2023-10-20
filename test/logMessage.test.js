52e37e1d17ded29ef603f8c3cde7f346a1158def
Davide Arena <arena.davide97@gmail.com>
2023-10-20 22:10:15 +0200

    chore: add test for uncovered usecases

--- test/logMessage.test.js
+++ test/logMessage.test.js
@@ -122,6 +122,88 @@ test('should log without msg using a logMessage function returning an undefined 
   })
 })
 
+test('should log without msg using a logMessage function returning an array containing non string value', async (t) => {
+  t.plan(5)
+
+  const customLogMessage = (context) => ['foobar', 3]
+
+  const stream = jsonLogger(
+    line => {
+      t.is(line.req, undefined)
+      t.is(line.reqId, 'req-1')
+      t.deepEqual(line.msg, undefined)
+      t.deepEqual(line.graphql, {
+        operationName: 'logMe',
+        queries: ['add', 'add', 'echo', 'counter']
+      })
+    })
+
+  const app = buildApp(t, { stream }, { logMessage: customLogMessage })
+
+  const query = `query logMe{
+    four: add(x: 2, y: 2)
+    six: add(x: 3, y: 3)
+    echo(msg: "hello")
+    counter
+  }`
+
+  const response = await app.inject({
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    url: '/graphql',
+    body: JSON.stringify({ query })
+  })
+  t.deepEqual(response.json(), {
+    data: {
+      four: 4,
+      six: 6,
+      echo: 'hellohello',
+      counter: 0
+    }
+  })
+})
+
+test('should log without msg using a logMessage function throwing an error', async (t) => {
+  t.plan(5)
+
+  const customLogMessage = (context) => { throw new Error() }
+
+  const stream = jsonLogger(
+    line => {
+      t.is(line.req, undefined)
+      t.is(line.reqId, 'req-1')
+      t.deepEqual(line.msg, undefined)
+      t.deepEqual(line.graphql, {
+        operationName: 'logMe',
+        queries: ['add', 'add', 'echo', 'counter']
+      })
+    })
+
+  const app = buildApp(t, { stream }, { logMessage: customLogMessage })
+
+  const query = `query logMe{
+    four: add(x: 2, y: 2)
+    six: add(x: 3, y: 3)
+    echo(msg: "hello")
+    counter
+  }`
+
+  const response = await app.inject({
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    url: '/graphql',
+    body: JSON.stringify({ query })
+  })
+  t.deepEqual(response.json(), {
+    data: {
+      four: 4,
+      six: 6,
+      echo: 'hellohello',
+      counter: 0
+    }
+  })
+})
+
 test('should log with msg using a logMessage function returning a string', async (t) => {
   t.plan(5)
 
